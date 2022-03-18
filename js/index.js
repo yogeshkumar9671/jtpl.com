275cca61de0da6ac4984d92bf5dff4e5e0268dda
yogeshkumar9671 <97506050+yogeshkumar9671@users.noreply.github.com>
2022-03-18 09:22:13 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,53 @@
+// Initialize Swiper
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+    keyboard: {
+        enabled: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
+
+
+
+
+
+
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 1,
+//     spaceBetween: 30,
+//     loop: true,
+//     autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: false,
+//       },
+
+//     breakpoints: {
+//         // when window width is >= 768px
+//         768:{
+//             slidesPerView: 2,
+//             spaceBetweenSlides: 30
+//         },
+//         // when window width is >= 1440px
+//         1440:{
+//             slidesPerView: 3,
+//             spaceBetweenSlides: 30
+//         },
+//     },
+
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+// });
